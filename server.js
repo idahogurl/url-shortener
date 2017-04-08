008f89f70a3385005b9ff481dcc523343d5fc91a
Rebecca Vest <vestre@Rebeccas-MacBook-Pro.local>
2017-04-08 00:17:16 -0600

    Implemented redirect and creation

--- server.js
+++ server.js
@@ -1,37 +1,115 @@
 var express = require("express");
 var mongodb = require("mongodb").MongoClient;
+var RSVP = require('rsvp');
+
 var app = express();
-
-app.get("/new?:url", function(req, res) {
-    mongodb.connect("mongodb://localhost:27017/local", function(err, database) {
-        var collection = database.collection("short_urls");
-        var id = getNextSequence(database, "url_id");
-        collection.insert({_id: id, url: req.query.url}, function(result) {
-            res.json({ "original_url":req.query.url, "short_url":"http://0.0.0.0/" + id });
-        });
-        
-        database.close();
-    });
+ 
+var promise = new RSVP.Promise(function(resolve, reject) {
+  mongodb.connect("mongodb://localhost:27017/local", function (err, database) {
+        console.log("here");
+        if (err) reject(err);
+        return resolve(database);
+    }); 
 });
-app.get("/:id", function(req, res){
-    mongodb.connect("mongodb://localhost:27017/local", function(err, database) {
-        var document = database.collection("short_urls").find({id: req.query.id });
-        res.redirect(document.url);
-    });
-});
+try {
+    app.get("/new?:url", function (req, res) {
+    
+    try {
+        promise.then(
+            function(database) 
+            {
+            
+            return database; 
+            }, 
+            function(err) 
+            {
+                res.json({error: err.message});
+            }
+        )
+        .then(
+            function(database)
+            {
+                var getNextSequence = new RSVP.Promise(function(resolve, reject) {
+                    //https://docs.mongodb.com/v3.0/tutorial/create-an-auto-incrementing-field/
+                    database.collection("counters").findAndModify({ _id: "url_id" }, [['_id', 'asc']], { $inc: { seq: 1 } }, { new: true, upsert: true }, function (err, doc) {
+                        if (err) reject(err);
+                    
+                        console.log(doc.value.seq);
+                        resolve({ database: database, id: doc.value.seq });
+                    });
+                });
+                return getNextSequence;
+            }
+        )
+        .then(
+            function(data){
+                var insert = new RSVP.Promise(
+                    function(resolve, reject) 
+                    {
+                        data.database.collection("short_urls")
+                            .insert({ _id: data.id, url: req.query.url },
+                                function (result)
+                                {
+                                    console.log(data.id);
+                                    if (result && result.hasWriteError()) reject(result.writeError.errmsg);
 
-//https://docs.mongodb.com/v3.0/tutorial/create-an-auto-incrementing-field/
-function getNextSequence(database, name) {
-    var ret = database.collection("counters").findAndModify(
-          
-    {_id: name},
-    [['_id','asc']],
-    { $inc: { seq: 1 } },
-    {new: true, upsert: true}, 
-    function(err, doc){
-        console.log('find and modified  ' +doc);
+                                    resolve(data.id);
+                                }
+                            );
+                    }
+                );
+                return insert;
+            }
+        )
+        .then(
+            function(id) 
+            {
+                res.json({ "original_url":req.query.url, "short_url":"http://0.0.0.0:3000/" + id });
+            }
+        )
+        .catch(
+            function(error) 
+            {
+                res.json({error: error.message});
+            }
+        );
     }
-  );
+    catch(err) 
+    {
+        res.json({error: error.message});
+    }
+    });
+} catch (err) {
+    console.log(err.message);
 }
 
-app.listen("3000", "0.0.0.0");
+app.get("/:id", 
+    function (req, res) 
+    {
+        try 
+        {
+            mongodb.connect("mongodb://localhost:27017/local", 
+                function (err, database) 
+                {
+                    var document = database.collection("short_urls").findOne({ id: req.params.id });
+                    document.then(function(result) {
+                        if (!result) 
+                        {
+                            res.sendStatus(404);
+                        } 
+                        else
+                        {
+                            res.redirect(result.url);
+                        }
+                });
+            });
+        } 
+        catch(err) 
+        {
+            console.log(err.message);
+        }
+    }
+);
+
+var port = "3000";
+app.listen(port, "127.0.0.1");
